b112c1b424abae6c05251fb9010dfb38ee64900f
ㅤZhxck <zhacxk@gmail.com>
2026-09-02 15:36:33 +0700

    feat(deploy): adjust allowed hosts and add whitenoise middleware for deployment

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myportofolio project.
+URL configuration for portofolio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from portofolio.views import landing_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page, name='landing_page'),
 ]
--- portofolio/wsgi.py
+++ portofolio/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myportofolio project.
+WSGI config for portofolio project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportofolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portofolio.settings')
 
 application = get_wsgi_application()
