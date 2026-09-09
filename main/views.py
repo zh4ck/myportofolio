00880571eb0eaa95a70ce66c958332e69e27709e
ㅤZhxck <zhacxk@gmail.com>
2026-09-09 17:37:26 +0700

    tasks(tutorial02): completed it's tasks and requirements

--- main/views.py
+++ main/views.py
@@ -5,12 +5,13 @@ from main.models import Experience
 
 def show_main(request):
     context = {
-        "name": "Burhan",
-        "npm": "2206000000",
-        "study_program": "S1 Ilmu Komputer",
+        "name": "Zayyan",
+        "npm": "2506550955",
+        "study_program": "S1 Sistem Informasi",
         "bio": (
-            "Mahasiswa Ilmu Komputer Universitas Indonesia yang tertarik "
-            "pada pengembangan perangkat lunak dan pendidikan."
+            """Passionate learners, aiming to become a polymath. Very interested in tinkering electronical devices and bootlegs. I spent my days experimenting with new things and creating something to be use for.
+            
+            Sometimes I play guitars, capturing moments, staring into the abysmal void of the universe, and most of the time reflecting on the meaning of life itself. The other time? probably chilling out, walking around the city."""
         ),
     }
     return render(request, "index.html", context)
@@ -18,7 +19,7 @@ def show_main(request):
 
 def show_experience(request):
     context = {
-        "name": "Burhan",
+        "name": "Zayyan",
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
--- portofolio/urls.py
+++ portofolio/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
-from portofolio.views import landing_page
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', landing_page, name='landing_page'),
+    path("admin/", admin.site.urls),
+    path("", include("main.urls")),
 ]
